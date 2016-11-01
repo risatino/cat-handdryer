a279b6f0b88054e4b3202af1b8c77152c64c0b74
risatino <charissatolentino@gmail.com>
2016-11-01 18:29:08 -0500

    i made a dryer

---
+++ lib/cat_handdryer.rb
@@ -0,0 +1,9 @@
+require "cat_handdryer/version"
+
+module CatHanddryer
+  class Dryer
+    def dry
+      `say oooooooooooaaaaah ah arrrrrrgaaaaaahhhhhhhhhhewwwwwwwwww wee`
+    end
+  end
+end
---
+++ lib/cat_handdryer/version.rb
@@ -0,0 +1,3 @@
+module CatHanddryer
+  VERSION = "0.1.0"
+end
